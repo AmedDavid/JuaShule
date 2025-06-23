9a3ae9e50cc6a44de2f4288a2a6ad9c33b6ce247
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-23 16:47:07 +0300

    modifies file Login,App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useAuth } from './context/AuthContext';
 import Home from './pages/Home';
+import Login from './pages/Login'
 // import Login from './pages/Login';
 // import Signup from './pages/Signup';
 import Navbar from './components/Navbar';
@@ -15,7 +16,7 @@ function App() {
           <main className="flex-grow container mx-auto px-4 py-8">
             <Routes>
               <Route path="/" element={<Home />} />
-              {/* <Route path="/login" element={<Login />} /> */}
+              {<Route path="/login" element={<Login />} /> }
               {/* <Route path="/signup" element={<Signup />} /> */}
             </Routes>
           </main>
