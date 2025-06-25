bc4955ea97cf987825757565935d5d99decf3e4d
David Amedi <amukoahdavid@gmail.com>
2025-06-25 13:18:52 +0300

    feat(config): add email configuration settings for Flask-Mail integration

--- server/app/config.py
+++ server/app/config.py
@@ -4,3 +4,11 @@ class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://juashule_user:Mchafuziarts189@localhost:5432/juashule'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = '5070a9eef46c97215a8c141e064b35128ded257eca6b12c16955967059a68e8a'
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'davidwize189@gmail.com'
+    MAIL_PASSWORD = 'gqsl abpc fzrz gvke'
+    MAIL_DEFAULT_SENDER = 'davidwize189@gmail.com'
+    MAIL_DEBUG = True
+    MAIL_SUPPRESS_SEND = False
