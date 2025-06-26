ff62b56e448121986c7a409b4fdee43e3d693c80
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-26 15:18:05 +0300

    modifies files in server side

--- server/app/config.py
+++ server/app/config.py
@@ -2,8 +2,8 @@ import os
 from datetime import timedelta
 
 class Config:
-    # SQLALCHEMY_DATABASE_URI = 'postgresql://juashule_db_user:Fjis1XgLjN3vrx68CBDRSQXDJmxrmJgb@dpg-d1dvlnjipnbc73dounrg-a.oregon-postgres.render.com/juashule_db'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://juashule_user:Mchafuziarts189@localhost:5432/juashule'
+    #SQLALCHEMY_DATABASE_URI = 'postgresql://juashule_db_user:Fjis1XgLjN3vrx68CBDRSQXDJmxrmJgb@dpg-d1dvlnjipnbc73dounrg-a.oregon-postgres.render.com/juashule_db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://juashule_user:Jaharistudio456@localhost:5432/juashule'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = '5070a9eef46c97215a8c141e064b35128ded257eca6b12c16955967059a68e8a'
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=6)
