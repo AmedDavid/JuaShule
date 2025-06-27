7b3f3984667e86ecba0fff2cdd83533627f308a3
Rochelle Awuor <awuorochelle75@gmail.com>
2025-06-27 21:52:20 +0300

    makes changes on files

--- server/run.py
+++ server/run.py
@@ -5,7 +5,7 @@ import os
 # internal url
 os.environ['DATABASE_URL'] = 'postgresql://juashule_db_user:Fjis1XgLjN3vrx68CBDRSQXDJmxrmJgb@dpg-d1dvlnjipnbc73dounrg-a/juashule_db'
 # local url
-# os.environ['DATABASE_URL'] = 'postgresql://juashule_user:Mchafuziarts189@localhost:5432/juashule'
+#os.environ['DATABASE_URL'] = 'postgresql://juashule_user:Mchafuziarts189@localhost:5432/juashule'
 os.environ['JWT_SECRET_KEY'] = '5070a9eef46c97215a8c141e064b35128ded257eca6b12c16955967059a68e8a'
 
 from app import create_app
